ae7b1287b423d234e5312ef84ce6078620f1530e
cinna <cinna9463@gmail.com>
2025-01-26 09:35:36 +0530

    need corrections

--- MulServer.c
+++ MulServer.c
@@ -1,128 +1,35 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-
 #include <pthread.h>
-#include <semaphore.h>
 
-#define Len 10
+#define PORT "5500"
+#define numclients 10
 
-typedef struct
-{
-    int clientSock;
-    char *msg;
-    int len;
-}clientDet;
+// Function to get the response file
+char* get_responseFile(char *filename, char *contype) {
+    char file[60];
+    strcpy(file, filename);
 
-clientDet clients[10];
-sem_t semaphore;
-
-void *clientResponse(void *args)
-{
-    clientDet *client = (clientDet *)args; 
-    int i=0;
-    while(1)
-    {
-        sem_wait(&semaphore);
-        char clientsent[500];
-        
-        int recvNo = recv(client[i].clientSock, clientsent, sizeof(clientsent), 0);
-        printf("message received = %s\n",clientsent);
-        printf("number of bytes received= %d\n",recvNo);
-
-        int sentNo = send(client[i].clientSock,client[i].msg, sizeof(char)*(client[i].len), 0);
-
-        printf("message sent ");
-        printf("number of bytes sent= %d\n\n",sentNo);
-
-        if(i==Len)
-        break;
-
-        i++;
-    }
-    pthread_exit(NULL);
-}
-
-int main()
-{
-
-    struct addrinfo hints, *servinfo;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    int result = getaddrinfo(NULL, "5000", &hints, &servinfo);
-
-    //check result and print server details
-    if(result ==0)
-    {
-        for(struct addrinfo *p=servinfo; p!=NULL; p=p->ai_next)
-        {
-            char ip[INET_ADDRSTRLEN];
-            if(p->ai_family == AF_INET)
-            {
-                struct sockaddr_in *addr4 = (struct sockaddr_in*)p->ai_addr;
-                inet_ntop(AF_INET, &(addr4->sin_addr), ip, INET6_ADDRSTRLEN);
-                printf("IPv4 address: %s\n", ip);
-            }
-            else if(p->ai_family == AF_INET6)
-            {
-                struct sockaddr_in6 *addr6 = (struct sockaddr_in6*)p->ai_addr;
-                inet_ntop(AF_INET6, &(addr6->sin6_addr), ip, INET6_ADDRSTRLEN);
-                printf("IPv6 address: %s\n", ip);
-            }
-        }
-    }
-    else
-    {
-        printf("getaddrinfo error!\n");
-        return -1;
+    if (strstr(filename, ".html") == NULL) {
+        strcat(filename, ".html"); // Automatically append .html if not present
     }
 
-    //server socket
-    int servSock = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-    
-    //initialize semaphore
-    sem_init(&semaphore, 0, 0);
+    printf("file name: %s\n", filename);
 
-    //initialize thread
-    pthread_t resThread;
-    pthread_create(&resThread, NULL, clientResponse, (void *)clients);
-
-    //bind to first address
-    for(struct addrinfo *p=servinfo; p!=NULL; p=p->ai_next)
-    {
-        result = bind(servSock, p->ai_addr, p->ai_addrlen);
-        if(result ==0)
-        {
-            printf("bind sucess\n");
-        }
-    }
-
-    //start listening at the address
-    result = listen(servSock,10);
-    if(result ==0)
-    printf("listening for connections...\n");
-
-    int i=0;
-    int clientSock;
-    
-    FILE *htmlFile;
-    htmlFile = fopen("Response.html", "r");
+    FILE *htmlFile = fopen(file, "rb");
     if (htmlFile == NULL) {
         perror("Error opening file");
-        return 1;
+        return NULL;
     }
 
-    int size = 0;
-    char *response;
-
+    long size = 0;
     fseek(htmlFile, 0, SEEK_END);
     size = ftell(htmlFile);
     rewind(htmlFile);
@@ -131,21 +38,21 @@ int main()
     if (buff == NULL) {
         perror("Memory allocation failed");
         fclose(htmlFile);
-        return 1;
+        return NULL;
     }
 
     fread(buff, 1, size, htmlFile);
     buff[size] = '\0';
     fclose(htmlFile);
 
-    char *httpProtocol = (char*)calloc(100,sizeof(char));
-    sprintf(httpProtocol, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: %d\r\n\r\n", size);
+    char *httpProtocol = (char*)calloc(100, sizeof(char));
+    sprintf(httpProtocol, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %ld\r\n\r\n", contype, size);
 
-    response = (char *)calloc(strlen(httpProtocol) + size + 1, sizeof(char));
+    char *response = (char *)calloc(strlen(httpProtocol) + size + 1, sizeof(char));
     if (response == NULL) {
         perror("Memory allocation failed");
         free(buff);
-        return 1;
+        return NULL;
     }
 
     strcpy(response, httpProtocol);
@@ -154,32 +61,186 @@ int main()
     free(httpProtocol);
     free(buff);
 
-    char *msg = response;
+    return response;
+}
 
-    while(1)
-    {
-        struct sockaddr *clientAddr;
-        int *addrlen = (int*)malloc(sizeof(int));
-        *addrlen = 0;
-        
-        clientSock = accept(servSock, clientAddr, addrlen);
-        
-        clientDet currClient = {clientSock, msg, strlen(msg)};    
-        clients[i] = currClient;
-        
-        if(i==Len-1)
-        {
+// Function to send the response file to client
+int sendresponseFile(int client_sock, char *filename, char *contype) {
+    char *response = get_responseFile(filename, contype);
+    if (response == NULL) {
+        response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: 0\r\n\r\n";
+        send(client_sock, response, strlen(response), 0);
+        return -1;
+    }
+
+    long sentNo = send(client_sock, response, strlen(response), 0);
+    if (sentNo == -1) {
+        perror("send error");
+        free(response);
+        return -1;
+    }
+
+    while (sentNo != strlen(response)) {
+        sentNo += send(client_sock, response + sentNo, strlen(response) - sentNo, 0);
+    }
+
+    free(response);
+    return sentNo;
+}
+
+// Function to get the route from the HTTP request
+char *getroute(char *request) {
+    char reqtype[30];
+    memset(reqtype, 0, sizeof(reqtype));
+
+    char *route = (char *)calloc(100, sizeof(char));
+    sscanf(request, "%s %s HTTP/1.1", reqtype, route);
+
+    return route;
+}
+
+// Function to get the request from the client
+char *getRequest(int client_sock) {
+    int buffsize = 1024;
+    char *clientsent = (char *)calloc(buffsize, sizeof(char));
+
+    int bytesread = 0;
+    int totalread = 0;
+
+    while (1) {
+        bytesread = recv(client_sock, clientsent + totalread, buffsize - totalread, 0);
+        if (bytesread < 0) {
+            perror("recv failed");
+            free(clientsent);
+            return NULL;
+        } else if (bytesread == 0) {
+            break; // Client disconnected
+        }
+
+        totalread += bytesread;
+
+        // Check if the full request has been received
+        if (strstr(clientsent, "\r\n\r\n")) {
+            break; // End of HTTP headers
+        }
+    }
+
+    clientsent[totalread] = '\0';
+    return clientsent;
+}
+
+// Function to handle the client response in a new thread
+void *clientResponse(void *args) {
+    int clientsock = *((int *)args);
+    free(args); // Free dynamically allocated memory for socket descriptor
+
+    char *clientsent = NULL, *route = NULL;
+
+    clientsent = getRequest(clientsock);
+    if (clientsent == NULL) {
+        printf("Error: Failed to receive the request from the client\n");
+        close(clientsock);
+        pthread_exit(NULL);
+    }
+
+    printf("Client sent:\n%s\n", clientsent);
+
+    route = getroute(clientsent);
+    if (route == NULL) {
+        printf("Error: Failed to extract route from the request\n");
+        free(clientsent);
+        close(clientsock);
+        pthread_exit(NULL);
+    }
+
+    printf("Requested route: %s\n", route);
+
+    // Route handling
+    if (strstr(route, "..") != NULL) {
+        // Prevent directory traversal
+        sendresponseFile(clientsock, "404.html", "text/html");
+    } else if (strcmp(route, "/") == 0) {
+        sendresponseFile(clientsock, "Response.html", "text/html");
+    } else if (strcmp(route, "/favicon.ico") == 0) {
+        sendresponseFile(clientsock, "favicon.ico", "image/x-icon");
+    } else {
+        sendresponseFile(clientsock, route + 1, "text/html"); // +1 to skip the leading "/"
+    }
+
+    free(route);
+    free(clientsent);
+
+    close(clientsock);
+    pthread_exit(NULL);
+}
+
+// Function to set up the server
+int serverSetup(char *port) {
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    struct addrinfo *servinfo;
+    int status;
+    if ((status = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(-1);
+    }
+
+    int sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+        perror("bind failed");
+        exit(-2);
+    }
+    else{
+        printf("bind sucess\n");
+    }
+
+    return sockfd;
+}
+
+int main() {
+    int servSock = serverSetup(PORT);
+
+    pthread_t resThread[numclients];
+
+    int result = listen(servSock, 10);
+    if (result == 0) {
+        printf("listening for connections...\n");
+    }
+
+    int i = 0;
+    struct sockaddr_in clientAddr;
+    int addrlen = sizeof(clientAddr);
+
+    while (1) {
+        int *clientSock = malloc(sizeof(int));
+        *clientSock = accept(servSock, (struct sockaddr *)&clientAddr, &addrlen);
+        if (*clientSock < 0) {
+            perror("Accept failed");
+            free(clientSock);
+            continue;
+        } else {
+            printf("client connected\n");
+        }
+
+        // Create a new thread for each client connection
+        pthread_create(&resThread[i], NULL, clientResponse, (void *)clientSock);
+
+        if (i == numclients - 1) {
             printf("max clients are served!\n");
-            pthread_cancel(resThread);
+            pthread_cancel(resThread[i]);
             return 0;
         }
         i++;
-
-        sem_post(&semaphore);
     }
 
-    pthread_join(resThread, NULL);
-
+    // Wait for all threads to complete
+    for (int j = 0; j < numclients; j++) {
+        pthread_join(resThread[j], NULL);
+    }
 
     return 0;
 }
--- SinServer.c
+++ SinServer.c
@@ -1,128 +1,159 @@
 #include <stdio.h>
-#include <string.h> // strerror()
+#include <string.h>    // strerror()
 #include <errno.h>
-#include <stdlib.h> // exit()
+#include <unistd.h>    // close()
+#include <stdlib.h>    // exit()
 #include <sys/socket.h> // sockets
 #include <sys/types.h> // type macros
 #include <netinet/in.h> // in_addr in6_addr sockaddr_in sockaddr_in6 
 #include <netdb.h>  // gives struct addrinfo and getaddrinfo()
 #include <arpa/inet.h> // for inet_ntop inet_ptons ptol etc network filling and conversions 
 
-int main()
-{
-    // stores address of info of a device or web etc
-    struct addrinfo hints;
-    hints.ai_family = AF_UNSPEC; // takes what ever ip version ipv4 or ipv6 
-    hints.ai_socktype = SOCK_STREAM; // tcp socket
-    hints.ai_flags = AI_PASSIVE; // filles our ip address
+#define PORT "5500" // Port defined as a string for getaddrinfo
 
-    struct addrinfo *servinfo;
+// Function to read the response file and construct an HTTP response
+char* get_responseFile(const char *filename) {
+    FILE *htmlFile = fopen(filename, "r");
+    if (htmlFile == NULL) {
+        perror("Error opening file");
+        return NULL;
+    }
+
+    // Get file size
+    fseek(htmlFile, 0, SEEK_END);
+    int size = ftell(htmlFile);
+    rewind(htmlFile);
+
+    char *buff = (char *)calloc(size + 1, sizeof(char));
+    if (buff == NULL) {
+        perror("Memory allocation failed");
+        fclose(htmlFile);
+        return NULL;
+    }
+
+    fread(buff, 1, size, htmlFile);
+    buff[size] = '\0';
+    fclose(htmlFile);
+
+    // Prepare the HTTP protocol header
+    char httpHeader[256];
+    snprintf(httpHeader, sizeof(httpHeader), 
+        "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: %d\r\n\r\n", size);
+
+    // Allocate memory for the full response
+    char *response = (char *)calloc(strlen(httpHeader) + size + 1, sizeof(char));
+    if (response == NULL) {
+        perror("Memory allocation failed");
+        free(buff);
+        return NULL;
+    }
+
+    strcpy(response, httpHeader);
+    strcat(response, buff);
+
+    free(buff);
+    return response;
+}
+
+int main() {
+    // Set up hints for getaddrinfo
+    struct addrinfo hints, *servinfo;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;      // IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP socket
+    hints.ai_flags = AI_PASSIVE;     // Use device's IP address
+
     int status;
-    //NULL means our device ip also can be given as "www.xyz.com",
-    // 5000 is port, hints is our given info,
-    // servinfo is all stuff filled version return
-    if((status = getaddrinfo(NULL, "5000", &hints, &servinfo)) != 0)
-    {
-        fprintf(stderr, "getaddinfo error: %s\n", gai_strerror(status));
-        exit(-1);
+    if ((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(EXIT_FAILURE);
     }
 
-    // lets print all the server details in linked list
-    for(struct addrinfo * PL = servinfo; PL != NULL; PL = PL->ai_next)
-    {
-        //ipv6 length can store both ipv6 and ipv4
-        char ALLtypeAddr[INET6_ADDRSTRLEN];
-        if(PL->ai_family == AF_INET)
-        {
-            struct sockaddr_in *addr4 = (struct sockaddr_in*)PL->ai_addr;
-            inet_ntop(AF_INET, &(addr4->sin_addr), ALLtypeAddr, INET6_ADDRSTRLEN);
-            printf("IPv4 address: %s\n", ALLtypeAddr);
-        }
-        else
-        {
-            struct sockaddr_in6 *addr6 = (struct sockaddr_in6*)PL->ai_addr;
-            inet_ntop(AF_INET6, &(addr6->sin6_addr), ALLtypeAddr, INET6_ADDRSTRLEN);
-            printf("IPv6 address: %s\n", ALLtypeAddr);
+    // Print available server addresses
+    for (struct addrinfo *pl = servinfo; pl != NULL; pl = pl->ai_next) {
+        char addr[INET6_ADDRSTRLEN];
+        if (pl->ai_family == AF_INET) {
+            struct sockaddr_in *addr4 = (struct sockaddr_in *)pl->ai_addr;
+            inet_ntop(AF_INET, &(addr4->sin_addr), addr, sizeof(addr));
+            printf("IPv4 address: %s\n", addr);
+        } else if (pl->ai_family == AF_INET6) {
+            struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *)pl->ai_addr;
+            inet_ntop(AF_INET6, &(addr6->sin6_addr), addr, sizeof(addr));
+            printf("IPv6 address: %s\n", addr);
         }
     }
 
-    /*struct sockaddr_in sa;
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(80);
-    memset(&sa.sin_zero,0,sizeof(sa.sin_zero));
-    
-    if( inet_pton(AF_INET, "10.12.110.57", &(sa.sin_addr)) < 0)
-    {
-        printf("ipv4 presentation to netwok unsuccesfull\n");
-        exit(-1);
-    }
-    struct sockaddr_in6 sa6;
-    sa6.sin6_family = AF_INET6;
-    sa6.sin6_port = htons(80);
-
-    if( inet_pton(AF_INET6, "2001:db8:63b3:1::3490", &(sa6.sin6_addr)) < 0)
-        printf("ipv6 presentation to network unsuccesfull\n");
-
-    //printf("size of char = %d",sizeof(char));
-
-
-    char sa_p[INET_ADDRSTRLEN];
-    char sa6_p[INET6_ADDRSTRLEN];
-
-    inet_ntop(AF_INET,&(sa.sin_addr), sa_p, INET_ADDRSTRLEN);
-    printf("presentation of ipv4 = %s\n",sa_p);
-
-    inet_ntop(AF_INET6,&(sa6.sin6_addr), sa6_p, INET6_ADDRSTRLEN);
-
-    printf("presentation of ipv6 = %s\n",sa6_p);*/
-
-    // socket is created with info and returns the file descriptor
+    // Create socket
     int sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-
-    // bind for servers to reserve port and listen for connections 
-    // now bind will associate or add the socket the ip info and reserves the port 
-    if(bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
-    {
-        printf("bind error: %s\n", strerror(errno));
-        exit(-2);
-    }
-    else 
-    {
-        printf("bind success\n");
+    if (sockfd == -1) {
+        perror("Socket creation failed");
+        freeaddrinfo(servinfo);
+        exit(EXIT_FAILURE);
     }
 
-    // gives the cliend address after accept
+    // Bind socket
+    if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+        perror("Bind error");
+        close(sockfd);
+        freeaddrinfo(servinfo);
+        exit(EXIT_FAILURE);
+    }
+    freeaddrinfo(servinfo); // No longer needed
+
+    // Listen for connections
+    if (listen(sockfd, 10) == -1) {
+        perror("Listen failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    printf("Server is listening on port %s...\n", PORT);
+
     struct sockaddr_storage clientAddr;
-    //size of client address intialized might be changed
-    socklen_t clientAddr_size = sizeof(clientAddr);
+    socklen_t clientAddrSize = sizeof(clientAddr);
 
-    // listen from out socket and queue for incoming connections is set to be 10
-    listen(sockfd, 10);
-
-    // accept a connection from queue and return new socket and client details
-    int client_sockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddr_size);
-
-    char msg[] = "hello client";
-    int sentno = 0;
-    
-    /*display the connected client details*/
-    {
-        char clientIp[16];
-        inet_ntop(AF_INET6, &((struct sockaddr_in6*)&clientAddr)->sin6_addr, clientIp,INET6_ADDRSTRLEN);
-        printf("client Ipv4 address= %s\n",clientIp);
-
-        inet_ntop(AF_INET, &((struct sockaddr_in*)&clientAddr)->sin_addr, clientIp,INET_ADDRSTRLEN);
-        printf("client Ipv6 address= %s\n",clientIp);
+    // Accept client connections
+    int clientSockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrSize);
+    if (clientSockfd == -1) {
+        perror("Accept failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
     }
-    
-    // send the message and returns the number of bytes sent
-    sentno = send(client_sockfd, msg, sizeof(msg), 0);
-    
-    printf("sent the msg = %s\n number of bytes sent = %d\n", msg, sentno);
 
-    close(client_sockfd);
+    // Display client IP address
+    char clientIp[INET6_ADDRSTRLEN];
+    if (clientAddr.ss_family == AF_INET) {
+        inet_ntop(AF_INET, &((struct sockaddr_in *)&clientAddr)->sin_addr, clientIp, sizeof(clientIp));
+        printf("Client IPv4 address: %s\n", clientIp);
+    } else if (clientAddr.ss_family == AF_INET6) {
+        inet_ntop(AF_INET6, &((struct sockaddr_in6 *)&clientAddr)->sin6_addr, clientIp, sizeof(clientIp));
+        printf("Client IPv6 address: %s\n", clientIp);
+    }
+
+    // Receive data from client
+    char buffer[1000];
+    memset(buffer, 0, sizeof(buffer));
+    int recvBytes = recv(clientSockfd, buffer, sizeof(buffer) - 1, 0);
+    if (recvBytes > 0) {
+        printf("Received message: %s\n", buffer);
+    } else {
+        perror("Receive failed");
+    }
+
+    // Send response to client
+    char *response = get_responseFile("Response.html");
+    if (response != NULL) {
+        int sentBytes = send(clientSockfd, response, strlen(response), 0);
+        if (sentBytes == -1) {
+            perror("Send failed");
+        }
+        free(response);
+    } else {
+        fprintf(stderr, "Failed to load response file\n");
+    }
+
+    // Close sockets
+    close(clientSockfd);
     close(sockfd);
-    free(servinfo);
 
     return 0;
 }
